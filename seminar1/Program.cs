11f61cc23c8612a21c06462261a0125dc0018558
Chesnokov M.A <chesnokov_ma@mail.ru>
2022-04-18 01:44:12 +0500

    Solution task 1, 2, 3

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -21,7 +21,7 @@ else
     }
 }
 */
-
+/*
 // Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Clear();
 Console.WriteLine("Введите первое число: ");
@@ -39,3 +39,16 @@ if(num3 > max)
     max = num3;
 
 Console.Write("Из чисел: " + num1 + ", " + num2 + ", " + num3 + " -> максимальное " + max);
+*/
+
+//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 2;
+
+if(result == 0)
+    Console.Write("Число: " + num + " -> четное");
+else
+    Console.Write("Число: " + num + " -> нечетное");
