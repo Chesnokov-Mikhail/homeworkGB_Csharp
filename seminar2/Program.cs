290171e223ac169bfee84cd70b11c6c2be4af2c5
Chesnokov M.A <chesnokov_ma@mail.ru>
2022-04-22 22:10:55 +0500

    Solution seminar2 task 10

---
+++ seminar2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+void SecondDigitThreeNumber(int num)
+{
+    int hundr = num%100;
+    Console.WriteLine("Вторая цифра числа:");
+    Console.Write(num + " -> " + hundr/10);
+}
+
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+SecondDigitThreeNumber(number);
