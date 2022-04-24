bb86693da7c31e788f0b02a6595512d690d049f2
Chesnokov M.A <chesnokov_ma@mail.ru>
2022-04-25 01:53:11 +0500

    Solution seminar4 task 34

---
+++ seminar4/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Clear();
+
+string PrintArrayInt(int[] arr)
+{
+    string res = "[";
+    for(int i = 0; i < (arr.Length - 1); i++)
+    {
+        res += Convert.ToString(arr[i]) + ", ";
+    }
+    res += Convert.ToString(arr[arr.Length - 1]);
+    res += "]";
+    return res;
+}
+
+int NumberEvenNumbers(int[] numbersArray)
+{
+    int count = 0;
+    for(int i = 0; i < numbersArray.Length; i++)
+    {
+        if(numbersArray[i] % 2 == 0)
+            count += 1;
+    }
+    return count;
+}
+Console.WriteLine("Введите размер массива трехзначных чисел:");
+int countArray = Convert.ToInt32(Console.ReadLine());
+int[] numbersArray = new int[countArray];
+for(int i =0; i < countArray; i++)
+{
+    numbersArray[i] = new Random().Next(100, 1000);
+}
+Console.WriteLine("{0} -> {1}", PrintArrayInt(numbersArray), NumberEvenNumbers(numbersArray));
